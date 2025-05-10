c132a80ed4c119380061355ffb93742922ab18fa
TopoCat <1452743919@qq.com>
2025-05-10 18:19:05 +0800

    完善Modbus从站工厂

--- ModbusCommunicator.Core/Interfaces/IModbusSlaveServiceFactory.cs
+++ ModbusCommunicator.Core/Interfaces/IModbusSlaveServiceFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModbusCommunicator.Core.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace ModbusCommunicator.Core.Interfaces
 {
     public interface IModbusSlaveServiceFactory
     {
-
+        IModbusSlaveService CreateSlaveService(SlaveListenerConfig slaveConfig);
     }
 }
