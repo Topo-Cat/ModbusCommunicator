cd61608e54e709a7aaf4a55be43f7368d8b25f70
TopoCat <1452743919@qq.com>
2025-05-10 11:09:59 +0800

    新增NModbusTcp模块，包装NModbus底层的Tcp实现模块

---
+++ ModbusCommunicator.NModbusTcp/ModbusMasterServiceFactory.cs
@@ -0,0 +1,15 @@
+﻿using ModbusCommunicator.Core.Interfaces;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusCommunicator.NModbusTcp
+{
+    public class ModbusMasterServiceFactory : IModbusMasterServiceFactory
+    {
+        IContainerProvider containerProvider;
+    }
+}
